6cb5649e61c401bb8a04211d237d99f469f9a454
MCaetan0 <marceloc.lima349@gmail.com>
2025-03-12 15:38:18 -0300

    feat:adiciona componentes no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import styles from "./App.module.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+import { ITask } from "./interfaces/Task";
 
 function App() {
+  const [taskList, settaskList] = useState<ITask[]>([]);
   return (
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteudo..</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm
+            btnText="Criar tarefa"
+            taskList={taskList}
+            setTaskList={settaskList}
+          />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList taskList={taskList} />
+        </div>
       </main>
       <Footer />
     </div>
