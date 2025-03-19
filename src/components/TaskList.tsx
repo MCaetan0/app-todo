46d7574da3e3f9010257cf72115856c2829059da
MCaetan0 <marceloc.lima349@gmail.com>
2025-03-19 15:54:49 -0300

    feat

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -6,21 +6,30 @@ import styles from "./TaskList.module.css";
 
 interface Props {
   taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdit(task: ITask): void;
 }
 
-const TaskList = ({ taskList }: Props) => {
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
   return (
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <div>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
-              <i className="bi bi-pencil"></i>
-              <i className="bi bi-trash"></i>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => handleEdit(task)}></i>
+              <i
+                className="bi bi-trash"
+                //no onClick sem arrow function o html tenta rodar isso já quando aparece na tela,
+                //não quando é clicado só
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
             </div>
           </div>
         ))
